7b4d29f34343cde2c94ae7d7b632a1c399bc5e8d
Waviis CR <Waviis CR>
2016-06-13 14:18:49 -0500

    prueba base

---
+++ app.js
@@ -0,0 +1,10 @@
+var clic = 1;
+function mostrarMenu(){ 
+   if(clic==1){
+   document.getElementById("menu").style.height = "4em";
+   clic = clic + 1;
+   } else{
+       document.getElementById("menu").style.height = "0px";      
+    clic = 1;
+   }   
+}
