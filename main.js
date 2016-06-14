d4c240bcf6ababf1ed9508768f5fe252bd068150
Waviis CR <Waviis CR>
2016-06-13 21:47:58 -0500

    cambiando a jquery

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var clic = 1;
 function mostrarMenu(){ 
    if(clic==1){
-   document.getElementById("menu").style.height = "30em";
-   document.getElementById("list-box").style.display = "none";
+   $("#menu").style.height = "30em";
+   $("#list-box").style.display = "none";
    clic = clic + 1;
    } else{
-       document.getElementById("menu").style.height = "0px";
-       document.getElementById("list-box").style.display = "inline";
+       $("#menu").style.height = "0px";
+       $("#list-box").style.display = "inline";
       
     clic = 1;
    }   
@@ -15,13 +15,13 @@ function mostrarMenu(){
 var clic = 1;
 function mostrarAside(){ 
    if(clic==1){
-   document.getElementById("aside").style.display = "block";
-      document.getElementById("list-box").style.display = "none";
+   $("#aside").style.display = "block";
+    $("#list-box").style.display = "none";
 
    clic = clic + 1;
    } else{
-       document.getElementById("aside").style.display = "none";
-       document.getElementById("list-box").style.display = "inline";
+       $("#aside").style.display = "none";
+       $("#list-box").style.display = "inline";
 
     clic = 1;
    }   
