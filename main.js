df72a2bc900f7eeaee12c3d1585404c0efcb7e6f
Waviis CR <Waviis CR>
2016-06-13 21:20:41 -0500

    testing

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 var clic = 1;
 function mostrarMenu(){ 
    if(clic==1){
-   document.getElementById("menu").style.height = "4em";
+   document.getElementById("menu").style.height = "30em";
+   document.getElementById("list-box").style.display = "none";
    clic = clic + 1;
    } else{
-       document.getElementById("menu").style.height = "0px";      
+       document.getElementById("menu").style.height = "0px";
+       document.getElementById("list-box").style.display = "inline";
+      
+    clic = 1;
+   }   
+};
+
+var clic = 1;
+function mostrarAside(){ 
+   if(clic==1){
+   document.getElementById("aside").style.display = "block";
+      document.getElementById("list-box").style.display = "none";
+
+   clic = clic + 1;
+   } else{
+       document.getElementById("aside").style.display = "none";
+       document.getElementById("list-box").style.display = "inline";
+
     clic = 1;
    }   
 };
